569465633c551d2f2b12615ca440e13704c0533c
Shubham2812 <shubham.aggarwal.1996@gmail.com>
2017-03-29 04:10:48 +0530

    Authentication Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users
+
+  get '/' => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  # <%= render "devise/shared/links" %>
+
 end
